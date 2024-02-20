4b8575e2b78fa119fbc2dd3c5ebf5c026484a623
github.com/pprimon <pprimon@live.com>
2024-02-20 16:55:18 -0300

    setup

--- apps/design-system/src/components/Button/Button.tsx
+++ apps/design-system/src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import { Button as ButtonMui} from '@mui/material';
 import {ButtonProps} from '@mui/material/Button'
 
 const Button = (props: ButtonProps) => (
-  <ButtonMui {...props}/>
+  <button {...props}/>
 )
 
 export default Button
---
+++ design-token/README.md
@@ -0,0 +1,7 @@
+# design-token
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build design-token` to build the library.
