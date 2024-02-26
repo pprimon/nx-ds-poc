23ddec467d934892973f5b37c02375f32a6b08f1
github.com/pprimon <pprimon@live.com>
2024-02-26 15:00:11 -0300

    start setup docker

--- apps/design-system/src/components/Button/Button.tsx
+++ apps/design-system/src/components/Button/Button.tsx
@@ -1,6 +1,10 @@
 import { Button as ButtonMui} from '@mui/material';
 import {ButtonProps} from '@mui/material/Button'
 
+
+// Use all except disableRipple
+// type ButtonBaseProps = Omit<MuiButtonProps, "disableRipple">;
+
 const Button = (props: ButtonProps) => (
   <ButtonMui {...props}/>
 )
